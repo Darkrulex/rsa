b565c1952aae7b15bf84b81ab28956a0252b2ed8
RSAPROGRAMMER <85215747+RSAPROGRAMMER@users.noreply.github.com>
2021-06-23 15:06:16 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -3,14 +3,7 @@ import os
 os.system ('termux-setup-storage')
 
 def yoyo():
-    print("")
-    print("")
-    print("\x1b[1;91m     AUTO UPDATE PROGRAM RUNNING ")
-    print("\x1b[1;92m")
-    print("")
-    os.system ('rm -rf rsa')
-    os.system ('git clone https://github.com/RSAPROGRAMMER/rsa')
-    os.system ('cd rsa')
+
     try:
         os.mkdir('/sdcard/RSA-PROGRAMMER')
     except OSError:
